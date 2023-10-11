5b5024765551a7a3e385d2d48d22e79a399eae8f
Ovidiu Valeanu <ovaleanu@amazon.co.uk>
2023-10-11 16:32:15 +0100

    feat: Variables for spire nodes & EKS API endpoints

--- examples/deploy-bookinfo.sh
+++ examples/deploy-bookinfo.sh
@@ -12,4 +12,4 @@ echo " >>>Check whether SPIRE has issued an identity to the workload"
 
 kubectl exec -i -t -n spire -c spire-server \
   "$(kubectl get pod -n spire -l app=spire-server -o jsonpath='{.items[0].metadata.name}')" \
-  -- ./bin/spire-server entry show
+  -- ./bin/spire-server entry show -socketPath /run/spire/sockets/server.sock
